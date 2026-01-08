614b095723b3dbe829a37a26900de476892233ab
shemxy <24031498@myrp.edu.sg>
2026-01-08 12:00:33 +0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,38 @@
+//include the required packages
+const express = require('express');
+const mysql = require('mysql2/promise');
+require('dotenv').config();
+const port = 3000;
+
+//database config info
+const dbConfig = {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    waitForConnections: true,
+    connectionLimit: 100,
+    queueLimit: 0,
+};
+
+//initialise Express app
+const app = express();
+//helps app to read JSON
+app.use(express.json());
+
+//start the server
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+});
+
+app.get('/menuitems', async (req, res) => {
+    try{
+        let connection = await mysql.createConnection(dbConfig);
+        const [rows] = await connection.execute('SELECT * FROM defaultdb.menu_items');
+        res.json(rows);
+    } catch(err) {
+        console.error(err);
+        res.status(500).json({message: 'Server error for menu items'});
+    }
+});
